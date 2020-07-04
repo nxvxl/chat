9c5c12013f3cedb22a1ec6b20750071241e144d3
noval <noval.dev@protonmail.com>
2020-07-04 14:07:42 +0700

    add footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <footer>
+      <small> Created by: <a href="https://github.com/nxvxl">noval</a> </small>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: 'App',
 };
 </script>
 
 <style>
-@import "./assets/style.css";
+@import './assets/style.css';
 </style>
